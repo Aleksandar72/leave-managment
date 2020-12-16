f40f52a84556fbd624c22ea3dc0106597c228c40
aleksandarruba <aleksandarruba@live.com>
2020-12-16 10:48:25 +0100

    Cleaning up generic repo

--- Contract/IRepositoryBase.cs
+++ Contract/IRepositoryBase.cs
@@ -6,16 +6,6 @@ using System.Threading.Tasks;
 
 namespace LeaveManagment.Contract
 {
-     public interface IRepositoryBase<T> where T : class
-    {
-       Task<ICollection<T>> FindAll();
-       Task<bool> isExist(int Id); 
-       Task<T> FindById(int Id);
-       Task<bool> Create(T entity);
-       Task<bool> Update(T entity);
-       Task<bool> Delete(T entity);
-       Task<bool> Save();
-    }
     public interface IGenericRepository<T> where T : class
     {
         Task<ICollection<T>> FindAll(
@@ -28,6 +18,6 @@ namespace LeaveManagment.Contract
         Task Create(T entity);
         void Update(T entity);
         void Delete(T entity);
-        // Task Save();
+        
     }
 }
